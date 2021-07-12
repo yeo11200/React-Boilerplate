546b93bd81c492b05c37d4ebce963ded7a6cf0f5
shinjinseop <yeo11200@hanmail.net>
2021-07-13 02:10:44 +0900

    [공통함수] 페이지네이션 추가 - 추가할 로직   - <, > 눌렀을 경우 원하는 페이지만큼 이동

--- src/component/common/Paging/Paging.jsx
+++ src/component/common/Paging/Paging.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
+import styled from './Paging.scss';
+
+const cx = classNames.bind(styled);
 
 const Paging = (props) => {
   const {totalCnt, nowCnt, wantCnt} = props
@@ -7,35 +11,39 @@ const Paging = (props) => {
     return wantCnt ? wantCnt : 10;
   }, [wantCnt]);
   
-  const pages = parseInt((totalCnt === 0) ? '1' : ((totalCnt -1) / want) + 1); // 소수점이 나올 경우, 정수형으로해서 비교를 해준다.
+  const pages = parseInt((totalCnt === 0 || !totalCnt) ? '1' : ((totalCnt - 1) / want) + 1); // 소수점이 나올 경우, 정수형으로해서 비교를 해준다.
+  const block = Math.ceil(1.0 * pageCnt / want); // 소수점을 반올림하는 함수
+  let end = block * want; // 원하는 갯수만큼 리턴
+  
+  if (end > pages) { // totalCnt의 갯수만큼 페이지가 왔다면, end를 페이지로 변환해서 더 큰 페이지가 나오기 방지
+    end = pages;
+  }
   
   const fnPageList = React.useCallback(() => {
     const page = [];
-    const block = Math.ceil(1.0 * pageCnt / want); // 소수점을 반올림하는 함수
     const first = (block -1) * want + 1;
-    let end = block * want; // 원하는 갯수만큼 리턴
-    
-    if (end > pages) { // totalCnt의 갯수만큼 페이지가 왔다면, end를 페이지로 변환해서 더 큰 페이지가 나오기 방지
-      end = pages;
-    }
+
     
     for (let i = first; i <= end; i++) {
+      const classname = i === pageCnt ? 'active' : '';
+      
       page.push(
-        <li>
+        <li className={cx(classname)}>
           {i}
         </li>
       )
     }
     
     return page;
-  }, [nowCnt]);
+  }, [pageCnt]);
   
+  console.log(pages, end);
   return (
-     <div>
+     <div className={cx('pagination')}>
          <ul>
-           <li>&lt;</li>
+           { pageCnt > 1 && <li>&lt;</li> }
            {fnPageList()}
-           <li>&gt;</li>
+           { pages > end && <li>&gt;</li> }
          </ul>
      </div>
   )
